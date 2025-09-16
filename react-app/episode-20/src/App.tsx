076dd2d42b14ed68ede23997ea492fcd29937073
Mayurs0 <mayurs@iconnectsolutions.com>
2025-09-16 18:16:46 +0530

    [LEARN]:React hooks

--- react-app/episode-20/src/App.tsx
+++ react-app/episode-20/src/App.tsx
@@ -1,4 +1,11 @@
-import React, { ReactNode, useCallback } from "react";
+import React, {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 
 import "./App.css";
 
@@ -20,16 +27,94 @@ const List: React.FC<{ items: string[]; onClick?: (item: string) => void }> = ({
     ))}
   </ul>
 );
+interface Payload {
+  text: string;
+}
+type ActionType =
+  | {
+      type: "ADD";
+      text: string;
+    }
+  | {
+      type: "REMOVE";
+      id: Number;
+    };
 function App() {
   const onlListClick = useCallback((item: string) => {
-   
     window.alert(item);
+  }, []);
+
+  const [payload, setPayload] = useState<Payload | null>(null);
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => setPayload(data));
+  }, []);
+
+  interface Todo {
+    id: number;
+    done: boolean;
+    text: string;
+  }
+
+  const [todos, dispatch] = useReducer(todoReducer, []);
+
+  function todoReducer(state: Todo[], action: ActionType): Todo[] {
+    switch (action.type) {
+      case "ADD":
+        return [
+          ...state,
+          {
+            id: state.length, // simple ID for demo
+            text: action.text,
+            done: false,
+          },
+        ];
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id);
+      default:
+        // TS ensures all cases handled, but for runtime safety:
+        throw new Error("Unknown action type");
+    }
+  }
+
+  const newTodoRef = useRef<HTMLInputElement>(null);
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      dispatch({
+        type: "ADD",
+        text: newTodoRef.current?.value,
+      });
+      newTodoRef.current.value = "";
+    }
   }, []);
   return (
     <div>
       <Heading title="Introduction" />
       <Box>Hello there</Box>
       <List items={["one", "two", "three"]} onClick={onlListClick} />
+      <Box>{JSON.stringify(payload)}</Box>
+
+      <Heading title="Todos" />
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {todo.text}
+          <button
+            onClick={() =>
+              dispatch({
+                type: "REMOVE",
+                id: todo.id,
+              })
+            }
+          >
+            Remove
+          </button>
+        </div>
+      ))}
+      <div>
+        <input type="text" ref={newTodoRef} />
+        <button onClick={onAddTodo}>ADD</button>
+      </div>
     </div>
   );
 }
